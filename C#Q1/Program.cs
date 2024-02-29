76b52cc40d609d3f2251a4d8c293a819e97809a6
piyush gupta <piyushgupta.pg2001@gmail.com>
2024-02-29 23:43:22 +0530

    remove bin and obj folder

---
+++ C#Q1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace HelloWorld
+{
+public class Program
+{
+public static void Main(string[] args)
+{
+string input=Console.ReadLine();
+long sum=0;
+while(input!="ok"){
+int t=0;
+sum+=Convert.ToInt32(input);
+input=Console.ReadLine();
+}
+Console.WriteLine(sum);
+}
+}
+}
